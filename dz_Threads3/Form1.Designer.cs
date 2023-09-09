71efab5f12336a4f5d7bf17679b280358d45ce61
Радченко Катерина <ketrinradchenko@gmail.com>
2023-09-09 15:07:09 +0300

    Add project files.

---
+++ dz_Threads3/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace dz_Threads3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxExtension = new System.Windows.Forms.TextBox();
+            this.textBoxSymbol = new System.Windows.Forms.TextBox();
+            this.comboBoxDisk = new System.Windows.Forms.ComboBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxExtension
+            // 
+            this.textBoxExtension.Location = new System.Drawing.Point(28, 44);
+            this.textBoxExtension.Name = "textBoxExtension";
+            this.textBoxExtension.Size = new System.Drawing.Size(185, 20);
+            this.textBoxExtension.TabIndex = 0;
+            // 
+            // textBoxSymbol
+            // 
+            this.textBoxSymbol.Location = new System.Drawing.Point(28, 127);
+            this.textBoxSymbol.Name = "textBoxSymbol";
+            this.textBoxSymbol.Size = new System.Drawing.Size(185, 20);
+            this.textBoxSymbol.TabIndex = 1;
+            // 
+            // comboBoxDisk
+            // 
+            this.comboBoxDisk.AutoCompleteCustomSource.AddRange(new string[] {
+            "C:",
+            "D:"});
+            this.comboBoxDisk.FormattingEnabled = true;
+            this.comboBoxDisk.Items.AddRange(new object[] {
+            "C:",
+            "D:"});
+            this.comboBoxDisk.Location = new System.Drawing.Point(370, 43);
+            this.comboBoxDisk.Name = "comboBoxDisk";
+            this.comboBoxDisk.Size = new System.Drawing.Size(127, 21);
+            this.comboBoxDisk.TabIndex = 2;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(370, 125);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(127, 23);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Искать";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(25, 169);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(604, 212);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя файла";
+            this.columnHeader1.Width = 178;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Папка";
+            this.columnHeader2.Width = 175;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Размер";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Дата последнего изменения";
+            this.columnHeader4.Width = 165;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(367, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 407);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.comboBoxDisk);
+            this.Controls.Add(this.textBoxSymbol);
+            this.Controls.Add(this.textBoxExtension);
+            this.Name = "Form1";
+            this.Text = "Поиск файлов на компьютере";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxExtension;
+        private System.Windows.Forms.TextBox textBoxSymbol;
+        private System.Windows.Forms.ComboBox comboBoxDisk;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ dz_Threads3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Remoting.Contexts;
+
+namespace dz_Threads3
+{
+    public partial class Form1 : Form
+    {
+        public static SynchronizationContext uiContext;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            uiContext = SynchronizationContext.Current;
+            label1.Text = "Введите расширение файла с точкой впереди " +
+                "\n(например .txt)" ;
+            label2.Text = "Введите параметр для поиска:" +
+                "\n * если ищие любые файлы с выбранным расширением" +
+                "\n ab? - если ищете определенные символы в названии";
+            label3.Text = "Выберите диск из выпадающего списка:";
+        }
+
+        // обработчик нажатия кнопки Искать
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string path;
+            // путь - выбранный через элемент comboBox диск
+            if (comboBoxDisk.SelectedIndex == 0)
+                path = "C:\\";
+            else
+                path = "D:\\";
+            // параметр для поиска - строка из двух textBox
+            string ex;
+            ex = textBoxSymbol.Text + textBoxExtension.Text;
+
+            // создаем объект структуры и передаем параметры для поиска
+            MyStruct myStruct = new MyStruct(path, ex);
+
+            // создаем объем поток, передаем делегат с параметрами и 
+            // инициализируем делегат методом Method
+            Thread thread = new Thread(new ParameterizedThreadStart(Method));
+            // объявляем поток фоновым
+            thread.IsBackground = true;
+            // запускаем поток, передаем в параметры структуру
+            thread.Start(myStruct);
+        }
+
+        // структура для того, чтобы можно было в параметризованный делегат
+        // передать две строки как один параметр
+        struct MyStruct  
+        {
+            public string filePath;  // путь к файлу (диск)  
+            public string symbol;    // расширение, символы для поиска
+            public MyStruct(string f, string s)
+            {
+                filePath = f;
+                symbol = s;
+            }
+        }
+
+        // метод для того, что метод для поиска файла упаковать в такой,
+        // какой подойдет для передачи в делегат ParameterizedThreadStart
+        public void Method(object obj)
+        {
+            MyStruct myStruct = (MyStruct)obj;
+            string path = myStruct.filePath;
+            string extension = myStruct.symbol;
+            // создаем список файлов типа FileInfo, инициализируем
+            // аго результатом рекурсивного метода для поиска
+            List<FileInfo> list = MyGetFiles(path, extension);
+
+            // в цикле ждя каждого файла показываем папку, размер,
+            // дату последнего обращения. 
+            foreach (var item in list)
+            {
+                // здесь имя файла передаем как конструктор ListViewItem
+                ListViewItem viewItem = new ListViewItem(item.Name);
+                // остальные параметры вызываем через свойства
+                viewItem.SubItems.Add(item.Directory.ToString());
+                viewItem.SubItems.Add(item.Length.ToString());
+                viewItem.SubItems.Add(item.LastAccessTime.ToString());
+                // с помощью контекста синхронизации записываем все
+                // в элемент listView
+                uiContext.Send(x=> listView1.Items.Add(viewItem), null);
+            }
+        }
+
+        // рекурсивынй метод для поиска файлов на диске компьютера
+        public  List<FileInfo> MyGetFiles(string path, string extension)
+        {
+            List<FileInfo> all = new List<FileInfo>();
+            try
+            {
+                foreach (var file in Directory.GetFiles(path, extension))
+                {
+                    all.Add(new FileInfo(file));
+                }
+                string[] directories = Directory.GetDirectories(path);
+                if (directories.Length > 0)
+                {
+                    foreach (string directory in directories)
+                    {
+                        all.AddRange(MyGetFiles(directory, extension));
+                    }
+                }
+            }
+            catch { }
+            return all;
+        }
+    }
+}
